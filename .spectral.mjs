9ae212edaa66ffb7a117135e689cb0df4db71f27
manaya sasaki <manaya.sasaki@slunker.jp>
2023-07-02 14:52:02 +0900

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwODgzMA/spectral.js?branch_slug=feat%2Fopenapi&token=60eaa239-b8a3-4b08-a0e1-ba9ce0f7cfa7";
+export default { extends: ruleset };
